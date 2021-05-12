f4911703c073a8834c57d5259650983a9169de10
Antonio Cucciniello <acucciniello@Antonios-MacBook-Pro.local>
2021-05-12 09:37:02 -0500

    Clicked the Log in With Google Option
    - I was overcomplicating it.
    - Now I am getting the frame, then from within the frame, access the text
    - TODO: Log in in the new window now!

--- test.js
+++ test.js
@@ -11,9 +11,12 @@ function sleep(ms) {
   await sleep(1000)
   await page.click('.login-wrapper button')
   await sleep(10000)
-  const loginIframe = await page.$('iframe')
-  const loginPage = await loginIframe.contentFrame()
-  await loginPage.click('.')
+  // get the iframes on the page, there should be two
+  const mainiFrames = await page.frames()
+  // select the second iframe which is the login windor
+  const loginFrame = mainiFrames[1]
+  // click the div so you can log in with Google!
+  await loginFrame.click('text=Log in with Google')
   await sleep(2000)
   // create pages, interact with UI elements, assert values
   await browser.close()
