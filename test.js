1c6298f6d5af3ac2026f60da5ee1c67688a0347b
Antonio Cucciniello <acucciniello@Antonios-MacBook-Pro.local>
2021-05-13 17:27:57 -0500

    Updated URL to go to home page
    - TODO: Implement the loop to pull analytics from each video

--- test.js
+++ test.js
@@ -11,39 +11,38 @@ function sleep(ms) {
   const page = await browser.newPage()
   await Promise.all([
     page.waitForNavigation(),
-    page.goto('https://tiktok.com')
+    page.goto('https://www.tiktok.com/@investarters?')
   ])  
-
   await browser.close()
 })()
 
 // not needed right now
-async function logInWithout2FA(page) {
-  new Promise((resolve, reject) => {
-    await page.click('text=Log in')
-    await sleep(1000)
-    // get the iframes on the page, there should be two
-    const mainiFrames = await page.frames()
-    // select the second iframe which is the login windor
-    const loginFrame = mainiFrames[1]
-    // click the div so you can log in with Google!
-    let [googleLogInPage] = await Promise.all([
-      browser.waitForEvent('page'),
-      loginFrame.click('text=Log in with Google')
-    ])
-    await googleLogInPage.waitForLoadState();
+// async function logInWithout2FA(page) {
+//   new Promise((resolve, reject) => {
+//     await page.click('text=Log in')
+//     await sleep(1000)
+//     // get the iframes on the page, there should be two
+//     const mainiFrames = await page.frames()
+//     // select the second iframe which is the login windor
+//     const loginFrame = mainiFrames[1]
+//     // click the div so you can log in with Google!
+//     let [googleLogInPage] = await Promise.all([
+//       browser.waitForEvent('page'),
+//       loginFrame.click('text=Log in with Google')
+//     ])
+//     await googleLogInPage.waitForLoadState();
   
-    await googleLogInPage.type('input', process.env.GOOGLE_USER, {delay: 100});
-    await Promise.all([
-      googleLogInPage.waitForNavigation(),
-      googleLogInPage.click("#identifierNext")
-    ])
-    await googleLogInPage.waitForLoadState()
-    console.log(process.env.GOOGLE_PWD)
-    await googleLogInPage.type('input', process.env.GOOGLE_PWD);
-    await sleep(2000)
-    await googleLogInPage.click("#passwordNext");
-    resolve()
-  })
+//     await googleLogInPage.type('input', process.env.GOOGLE_USER, {delay: 100});
+//     await Promise.all([
+//       googleLogInPage.waitForNavigation(),
+//       googleLogInPage.click("#identifierNext")
+//     ])
+//     await googleLogInPage.waitForLoadState()
+//     console.log(process.env.GOOGLE_PWD)
+//     await googleLogInPage.type('input', process.env.GOOGLE_PWD);
+//     await sleep(2000)
+//     await googleLogInPage.click("#passwordNext");
+//     resolve()
+//   })
   
-}
+// }
