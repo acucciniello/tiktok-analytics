20d83005d8ad34ddcb12e6baba237539add9bbdc
Antonio Cucciniello <acucciniello@Antonios-MacBook-Pro.local>
2021-05-13 13:53:08 -0500

    Log In Code Separated - log in manually and store the information - TODO: start looking at how to click on profile page and then loop through videos

--- test.js
+++ test.js
@@ -1,23 +1,49 @@
-const { chromium } = require('playwright');
+
+require('dotenv').config()
+const { chromium, firefox } = require('playwright');
 
 function sleep(ms) {
   return new Promise(resolve => setTimeout(resolve, ms))
 }
 
 (async () => {
-  const browser = await chromium.launch({headless: false, slowMo: 185});
+  const browser = await firefox.launchPersistentContext('antonio-firefox', {headless: false, slowMo: 185});
   const page = await browser.newPage()
-  await page.goto('https://tiktok.com')
-  await sleep(1000)
-  await page.click('.login-wrapper button')
-  await sleep(10000)
-  // get the iframes on the page, there should be two
-  const mainiFrames = await page.frames()
-  // select the second iframe which is the login windor
-  const loginFrame = mainiFrames[1]
-  // click the div so you can log in with Google!
-  await loginFrame.click('text=Log in with Google')
-  await sleep(2000)
-  // create pages, interact with UI elements, assert values
+  await Promise.all([
+    page.waitForNavigation(),
+    page.goto('https://tiktok.com')
+  ])  
+
   await browser.close()
 })()
+
+// not needed right now
+async function logInWithout2FA(page) {
+  new Promise((resolve, reject) => {
+    await page.click('text=Log in')
+    await sleep(1000)
+    // get the iframes on the page, there should be two
+    const mainiFrames = await page.frames()
+    // select the second iframe which is the login windor
+    const loginFrame = mainiFrames[1]
+    // click the div so you can log in with Google!
+    let [googleLogInPage] = await Promise.all([
+      browser.waitForEvent('page'),
+      loginFrame.click('text=Log in with Google')
+    ])
+    await googleLogInPage.waitForLoadState();
+  
+    await googleLogInPage.type('input', process.env.GOOGLE_USER, {delay: 100});
+    await Promise.all([
+      googleLogInPage.waitForNavigation(),
+      googleLogInPage.click("#identifierNext")
+    ])
+    await googleLogInPage.waitForLoadState()
+    console.log(process.env.GOOGLE_PWD)
+    await googleLogInPage.type('input', process.env.GOOGLE_PWD);
+    await sleep(2000)
+    await googleLogInPage.click("#passwordNext");
+    resolve()
+  })
+  
+}
